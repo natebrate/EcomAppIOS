29040fbe9e23f0a3786ba115e8cf0c8a1816ed1c
natebrate <nbrathwaite16@gmail.com>
2021-05-20 12:09:14 -0400

    initialization

--- react/ViewController.swift
+++ react/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
+        
+        let myURL = URL(string:"https://seaside-live.herokuapp.com/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }}
 
